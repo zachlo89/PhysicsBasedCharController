3da36b0fd9f81c86e429e1f82646cdde4310ddce
Zach Lo <zachlo89@gmail.com>
2020-08-10 18:25:58 -0700

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _charController;
+    private Vector3 _direction;
+    private Vector3 _velocity;
+
+    [SerializeField] private float _gravity = 1.0f; // in real world val is -9.81f
+    [SerializeField] private float _speed = 5.0f; // meters per sec
+    [SerializeField] private float _jumpHeight = 15.0f;
+    [SerializeField] private float _yVelocity; // var to cache y velocity to another var
+
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    void FixedUpdate()
+    {
+        // 1 do all calcs..
+        // get horizontal input
+        float _horizontalInput = Input.GetAxis("Horizontal");
+
+        // define direction based on this input
+        _direction = new Vector3(_horizontalInput, 0, 0);
+
+        // define velocity
+        _velocity = _direction * _speed;
+
+        // after apply velocity, chk if grounded.
+        if (_charController.isGrounded == true)
+        {
+            // if i hit space jump; define jump height
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight; // set y velocity to 15
+            }
+        }
+        else
+        {
+            // apply gravity
+            _yVelocity -= _gravity;
+        }
+        // cached vel = 15; goes back up to line 25 calc next fr
+        // cached val is 14 cuz subtracted 1 from gravity
+        _velocity.y = _yVelocity; 
+
+        // 2 after calcs update...
+        // call move method; MOVE based on the direction
+        _charController.Move(_velocity * Time.deltaTime);
+    }
+}
